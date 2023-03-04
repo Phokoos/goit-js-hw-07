ef20795e0cd0e8cb2f2ec2008d860c75652c76d0
Phokoos <hulkovskij@gmail.com>
2023-03-04 17:13:03 +0200

    Finished homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const newGalleryEl = galleryItems.map(({preview, original, description}) =>
+	`<div class="gallery__item">
+		<a class="gallery__link" href="${original}">
+    		<img
+      		class="gallery__image"
+      		src="${preview}"
+      		data-source="${original}"
+      		alt="${description}"/>
+	  </a>
+	</div>`).join("");
+
+galleryEl.innerHTML = newGalleryEl;
+
+galleryEl.addEventListener("click", (event) => {
+	event.preventDefault();
+
+	if (event.target.nodeName !== "IMG") {
+    return;
+	}
+	const imgUrl = event.target.dataset.source;
+
+	return showImgModal(imgUrl);
+})
+
+const showImgModal = (imgUrl) => {
+	const instance = basicLightbox.create(`<img src="${imgUrl}" width="1280" height="853">`);
+
+	instance.show();
+
+	document.addEventListener("keyup", (keyCode) => imgClose(keyCode), {once:true});
+	
+	const imgClose = (keyCode) => {
+		if ("Escape" === keyCode.code) {
+			instance.close();
+		}
+	}
+} 
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const newGalleryEl = galleryItems.map(({preview, original, description}) =>
+	`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`).join("");
+
+galleryEl.innerHTML = newGalleryEl;
+
+new SimpleLightbox('.gallery a', {
+	captions: true,
+	captionsData: 'alt',
+	captionPosition: 'bottom',
+	captionDelay: 250,
+});
